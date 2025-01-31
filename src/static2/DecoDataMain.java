5fa76bce96554c5c6324951fc1bdb7663cc8ec9d
Aunar614 <developkim614@gmail.com>
2025-01-31 20:48:21 +0530

    문제와 풀이

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,14 @@
 package static2;
 
+// import static static2.DecoData.staticCall;
+// import static static2.DecoData.*;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
         DecoData.staticCall();
+
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -16,5 +20,15 @@ public class DecoDataMain {
 
         System.out.println("3. 정적 호출 (참조값 첨부)");
         DecoData.staticCall(data1);
+
+        // 추가
+        // 인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        System.out.println("4. 인스턴스를 통한 접근");
+        data3.staticCall();
+
+        // 클래스를 통한 접근
+        System.out.println("5. 클래스를 통한 접근");
+        DecoData.staticCall();
     }
 }
