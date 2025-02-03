fc4defac222158f2d75081164201e7571b6d1c21
Aunar614 <developkim614@gmail.com>
2025-02-03 12:38:52 +0530

    final 변수와 상수

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    /* 변수 선언이 되어서 생성자에서는 선언 불가
+    public FieldInit(int value) {
+        this.value = value;
+    }
+     */
+}
